83220e99844d16548f62f1b4248c9ecd91174409
Cody Heiser <codyheiser49@gmail.com>
2018-05-06 15:13:04 -0500

    fine tuning Mac OSX profile and aliases

--- osx_bash_aliases.sh
+++ osx_bash_aliases.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# terminal color preferences
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
 
+# git preferences
+source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
+GIT_PS1_SHOWDIRTYSTATE=true
+
+# prompt
+#export PS1="[\h: \w$ (__git_ps1)]\n$ "
+
+# TMUX
 alias tmxs='tmux switch -t $l'
 alias tmxa='tmux attach -t $l'
 alias tmxd='tmux detach'
@@ -10,15 +19,19 @@ alias tmxl='tmux ls'
 alias tmxk='tmux kill-window -t $l'
 alias tmxn='tmux new -s $l'
 
+# EMACS
 alias emacs='emacs -nw'
 
+# navigation
 alias b='cd ..'
 alias ..='cd ..'
 alias cd..='cd ..'
 
+# LS options
 alias ls='ls -F'
 alias la='ls -a'
 alias lal='ls -al'
 
+# grep options
 alias grep='grep --color'
 alias grepl='grep -lr'
